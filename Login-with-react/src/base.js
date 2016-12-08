ad64a752f066e85801067b3f635f9fc28c0cc6b1
Mika <mika@purplerockscissors.com>
2016-12-08 09:24:49 -0500

    First Commit

--- Login-with-react/src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-    apiKey: "AIzaSyDC0lDx3SbJjZyGsN7WOlqKcUL40RtUP8U",
-    authDomain: "catch-of-the-day-mika-gurrola.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-mika-gurrola.firebaseio.com"
-});
-
-export default base;
--- Login-with-react/src/index.js
+++ Login-with-react/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Match, Miss } from 'react-router';
+// import { BrowserRouter, Match, Miss } from 'react-router';
+import { BrowserRouter, Match } from 'react-router';
 
 import './css/style.css';
 import App from './components/App';
-import StorePicker from './components/StorePicker';
-import NotFound from './components/NotFound';
 
 const Root = () => {
 	return (
 		<BrowserRouter>
 			<div>
-				<Match exactly pattern="/" component={StorePicker} />
-				<Match pattern="/store/:storeId" component={App} />
-				<Miss component={NotFound} />
+				<Match exactly pattern="/" component={App} />
 			</div>
 		</BrowserRouter>
 	)
